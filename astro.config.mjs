25c6994061956801013168a3ef865815119c73a7
ukefat <ukefat@gmail.com>
2026-01-08 14:30:30 -0700

    removed github page adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,11 @@
 import { defineConfig } from 'astro/config';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
-import github from '@astrojs/github';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
   site: 'https://ukefat.github.io/',
-  adapter: github(),
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
